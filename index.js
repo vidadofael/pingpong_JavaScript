b7891fedaa15330469be66abb70e005cd35eec7e
raphadev87 <raphaeldiasdev@gmail.com>
2024-01-08 20:17:36 -0300

    drawing the left racket of the game | drawing the right racket of the game

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const canvasEl = document.getElementById("canvas"),
     canvasCtx = canvas.getContext("2d"),
     collorField = "#015E4D",
-    collorCenterLine = "#FFFFFF";
+    whiteCollor = "#FFFFFF";
     const lineWidth = 15;
+    const widthRackets = lineWidth;
+    const heightRackets = 200;
 
 function setUp() {
   canvasEl.width = canvasCtx.width = window.innerWidth;
@@ -15,8 +17,17 @@ function draw() {
     canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
     //drawing the center line
-    canvasCtx.fillStyle = collorCenterLine;
+    canvasCtx.fillStyle = whiteCollor;
     canvasCtx.fillRect(window.innerWidth / 2 - lineWidth / 2, 0, lineWidth, window.innerHeight);
+
+    //drawing the left racket of the game
+    canvasCtx.fillStyle = whiteCollor;
+    canvasCtx.fillRect(10, 400, widthRackets, heightRackets)
+
+    //drawing the right racket of the game
+    canvasCtx.fillStyle = whiteCollor;
+    canvasCtx.fillRect(window.innerWidth - lineWidth - 10, 600, widthRackets, heightRackets)
+
 };
 
 //calling functions
