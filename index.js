e9aacc21ae3d8a332c7e6ba64c3ca8e3ecc5e59c
raphadev87 <raphaeldiasdev@gmail.com>
2024-01-08 19:12:50 -0300

    positioning field on screen using screen properties

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const canvasEl = document.getElementById("canvas"),
-        canvasCtx = canvas.getContext("2d")
+  canvasCtx = canvas.getContext("2d");
+
+const collorField = "#00745F";
+
+function setUp() {
+  canvasEl.width = canvasCtx.width = window.innerWidth;
+  canvasEl.height = canvasCtx.height = window.innerHeight;
+};
+
+//drawing rectangle
 
 
 
+function draw() {
+    canvasCtx.fillStyle = collorField;
+    canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+};
 
-
-
-        function setUp() {
-            canvasEl.width = canvasCtx.width = window.innerWidth
-            canvasEl.height = canvasCtx.height = window.innerHeight
-        };
-
-        function draw() {
-
-        };
-
-
-        //chamando funções
-        setUp()
+//calling functions
+setUp();
+draw();
