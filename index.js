b9c15cd4c0b3e85f95f34543860355803aed58f6
raphadev87 <raphaeldiasdev@gmail.com>
2024-01-08 18:57:37 -0300

    creating new JS file

---
+++ index.js
@@ -0,0 +1,20 @@
+const canvasEl = document.getElementById("canvas"),
+        canvasCtx = canvas.getContext("2d")
+
+
+
+
+
+
+        function setUp() {
+            canvasEl.width = canvasCtx.width = window.innerWidth
+            canvasEl.height = canvasCtx.height = window.innerHeight
+        };
+
+        function draw() {
+
+        };
+
+
+        //chamando funções
+        setUp()
